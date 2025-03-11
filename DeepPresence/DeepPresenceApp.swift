bf31559829d4c89d3c8103fde8e6e11e517b3052
Abhay Shakya <abhay24108@iiitd.ac.in>
2025-03-11 20:26:01 +0530

    Initial commit

---
+++ DeepPresence/DeepPresenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepPresenceApp.swift
+//  DeepPresence
+//
+//  Created by Abhay Shakya on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepPresenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeepPresenceTests/DeepPresenceTests.swift
@@ -0,0 +1,16 @@
+//
+//  DeepPresenceTests.swift
+//  DeepPresenceTests
+//
+//  Created by Abhay Shakya on 3/11/25.
+//
+
+import Testing
+
+struct DeepPresenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
